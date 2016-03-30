cbc983b5f6ae5c469b4c3753976a9c8fd59b26cf
Anna Clawson <seadeer@gmail.com>
2016-03-30 14:46:27 -0700

    initial commit (setting up environment)

---
+++ djangoenv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/aclawson/code/codingdojo/python/django/hackathon_Mar31/djangoenv/bin/python2.7
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
