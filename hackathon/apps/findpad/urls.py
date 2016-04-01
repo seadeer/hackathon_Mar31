673e213b0c15846beca74ce8765549602a04aa65
Anna Clawson <seadeer@gmail.com>
2016-04-01 09:54:25 -0700

    cleaned repo to get rid of bad db

--- hackathon/apps/findpad/urls.py
+++
@@ -1,5 +0,0 @@
-from django.conf.urls import patterns, url
-from apps.findpad import views
-urlpatterns = patterns('',
-	url(r'^$', views.add, name='add'),
-)
--- hackathon/hackathon/urls.py
+++
@@ -1,8 +0,0 @@
-from django.conf.urls import url, include
-from django.contrib import admin
-
-urlpatterns = [
-   url(r'^admin/', admin.site.urls),
-   url(r'^findpad/', include('apps.findpad.urls')),
-   url(r'^', include('apps.logreg.urls')),
-]
