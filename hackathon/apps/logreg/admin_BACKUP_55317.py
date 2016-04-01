c8ec1586f7422bcb9a917070b5a111be43d1283c
Anna Clawson <seadeer@gmail.com>
2016-03-31 17:39:33 -0700

    remove debris

--- hackathon/apps/logreg/admin_BACKUP_55317.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-<<<<<<< .merge_file_81xpyo
-
-=======
-from .models import User
->>>>>>> .merge_file_xDuVzO
-# Register your models here.
-admin.site.register(User)
