a8f0c9ff650aedf9531210953a37652b29774b56
Anna Clawson <seadeer@gmail.com>
2016-04-01 11:41:24 -0700

    added unicode method to the model for admin

--- hackathon/apps/logreg/models.py
+++ hackathon/apps/logreg/models.py
@@ -8,3 +8,5 @@ class Profile(models.Model):
     user = models.ForeignKey(User)
     oauth_token=models.CharField(max_length=200)
     oauth_secret = models.CharField(max_length=200)
+    def __unicode__(self):
+        return unicode(self.user)
